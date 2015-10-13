f6d154e98653de9bdee0a23b130847401696f999
noroach <noroach@bl-soic-9gs8w12.ads.iu.edu>
2015-10-13 12:56:57 -0400

    do it pls
    First Build
    
    Added all the needed starter content

---
+++ Fall-2015/UnrealEngineStuff/BatteryCollectorTutorial/BatteryCollector/Intermediate/Build/Win64/UE4Editor/Development/BatteryCollector/BatteryCollector.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_BATTERYCOLLECTOR_H__
+#define __AUTO_BATTERYCOLLECTOR_H__
+//Last Write: 10/13/2015 2:56:16 PM +00:00
+#include "C:\Users\noroach\Desktop\GameDevelopment\Fall-2015\UnrealEngineStuff\BatteryCollectorTutorial\BatteryCollector\Source\BatteryCollector\BatteryCollector.h"
+#endif//__AUTO_BATTERYCOLLECTOR_H__
---
+++ Fall-2015/UnrealEngineStuff/BatteryCollectorTutorial/BatteryCollector/Intermediate/Build/Win64/UE4Editor/Development/BatteryCollector/PCH.BatteryCollector.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\noroach\Desktop\GameDevelopment\Fall-2015\UnrealEngineStuff\BatteryCollectorTutorial\BatteryCollector\Source\BatteryCollector\BatteryCollector.h"
